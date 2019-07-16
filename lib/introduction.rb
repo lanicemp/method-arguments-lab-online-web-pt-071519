788a7e57c6b8db124d2f73dce04062144a43889b
LaNice Powell <lanicemp@aol.com>
2019-07-16 02:07:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end 
 
-def intoduction_with_language(name, language)
+def intoduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to proram in #{language}".
 end 
